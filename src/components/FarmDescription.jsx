30a6646018c0f45111510a7bf516d3c200aa7444
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2020-11-07 01:54:00 +0200

    finish farm description component

--- src/components/FarmDescription.jsx
+++ src/components/FarmDescription.jsx
@@ -1,15 +1,36 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, CardHeader, Typography } from '@material-ui/core';
+import farmerImg from '../assets/images/farmer.jpg'
+import { Card, CardContent, CardHeader, Typography, Avatar } from '@material-ui/core';
 
-const useStyles = makeStyles({
-
-})
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginTop: '100px',
+    overflow: 'visible'
+  },
+  avatar: {
+    width: '200px',
+    height: '200px',
+    margin: '16px',
+    marginTop: '-100px',
+  },
+  headerContainer: {
+    display: 'flex',
+  },
+  cardHeader: {
+    width: '70%'
+  }
+}));
 
 const FarmDescription = (props) => {
+  const classes = useStyles();
   const farm = props.farm;
   return (
-    <Card>
-      <CardHeader title={farm.farmName} subheader={farm.farmLocation} />
+    <Card className={classes.card}>
+      <div className={classes.headerContainer}>
+        <CardHeader title={farm.farmName} subheader={farm.farmLocation} className={classes.cardHeader} />
+        <Avatar src={farmerImg} className={classes.avatar} />
+      </div>
+
       <CardContent>
         <Typography paragraph>
           {farm.farmDescription}
