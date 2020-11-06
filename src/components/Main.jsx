38b798421511d7510dabef8e246aceded876ebe4
Petro Silenius <pvsile@utu.fi>
2020-11-06 23:50:55 +0200

    Add initial styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,27 @@
-import { Container, Grid } from '@material-ui/core';
+import { Container, Grid, makeStyles } from '@material-ui/core';
 import Search from './Search';
 
+const useStyles = makeStyles({
+  background: {
+    background: '#DCEAF9',
+    height: '100vh',
+    width: '100vw',
+  },
+  container: {
+    padding: '10px',
+  },
+});
+
 function Main() {
+  const classes = useStyles();
   return (
-    <Container maxWidth="sm">
-      <Grid>
-        <Search />
-      </Grid>
-    </Container>
+    <div className={classes.background}>
+      <Container maxWidth="sm" className={classes.container}>
+        <Grid>
+          <Search />
+        </Grid>
+      </Container>
+    </div>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './index.css';
 import Main from './components/Main';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Main />
-        </Route>
-      </Switch>
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/">
+        <Main />
+      </Route>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
