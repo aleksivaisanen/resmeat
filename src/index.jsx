e3b62550a34f4e8e966d1425e5192b09c54db477
Petro Silenius <pvsile@utu.fi>
2020-11-07 11:47:08 +0200

    Add routing and main view

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { ThemeProvider, CssBaseline, Container, makeStyles } from '@material-ui/core';
 import theme from './theme';
 import Main from './views/Main';
+import Farm from './views/Farm';
+import Product from './views/Product';
+
+const useStyles = makeStyles({
+  background: {
+    background: '#EEF5FC',
+    height: '100%',
+    minHeight: '100vh',
+    width: '100%',
+  },
+  container: {
+    paddingTop: '20px',
+  },
+});
+
+function Page({ children }) {
+  const classes = useStyles();
+  return (
+    <div className={classes.background}>
+      <Container maxWidth="sm" className={classes.container}>
+        {children}
+      </Container>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <Router>
       <Switch>
-        <Route path="/">
-          <Main />
-        </Route>
+        <Page>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/farm/:farmid">
+            <Farm />
+          </Route>
+          <Route path="/product/:productid">
+            <Product />
+          </Route>
+        </Page>
       </Switch>
     </Router>
   </ThemeProvider>,
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,25 +1,35 @@
+<<<<<<< Updated upstream
 import { Container, Grid, makeStyles, Card } from '@material-ui/core';
 import Search from '../components/Search';
 import PieCard from '../components/PieCard';
 import FarmDescription from '../components/FarmDescription';
+=======
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+>>>>>>> Stashed changes
 import ProductCard from '../components/ProductCard';
-import data from '../data/data.json'
+import logo from '../assets/images/logo.png';
 
 const useStyles = makeStyles({
+<<<<<<< Updated upstream
   background: {
     background: '#DCEAF9',
     height: '100vh',
     width: '100%',
+=======
+  headerGrid: {
+    textAlign: 'center',
+    marginTop: '20px',
+>>>>>>> Stashed changes
   },
-  container: {
-    paddingTop: '20px',
+  headerImg: {
+    marginTop: '20px',
   },
 });
 
 function Main() {
   const classes = useStyles();
-  const jsonData = data[0];
   return (
+<<<<<<< Updated upstream
     <div className={classes.background}>
       <Container maxWidth="sm" className={classes.container}>
         <Grid container item xs={12}>
@@ -43,6 +53,29 @@ function Main() {
         </Grid>
       </Container>
     </div>
+=======
+    <Grid container xs={12} spacing={2}>
+      <Grid item xs={12} className={classes.headerGrid}>
+        <Typography variant="h4" align="center">
+          {'Welcome to resmeat! Scan a products QR to see its responsibility factors'}
+        </Typography>
+        <img
+          src={logo}
+          alt="resmeat-logo"
+          height="100px"
+          width="100px"
+          className={classes.headerImg}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6">{'Last scanned'}</Typography>
+        <ProductCard />
+      </Grid>
+      <Grid item xs={12}>
+        <ProductCard />
+      </Grid>
+    </Grid>
+>>>>>>> Stashed changes
   );
 }
 
