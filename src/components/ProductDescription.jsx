06682dae8a1bba84d41b959cef847c5b2950a3b6
Petro Silenius <pvsile@utu.fi>
2020-11-07 14:08:00 +0200

    Add product page

---
+++ src/components/ProductDescription.jsx
@@ -0,0 +1,45 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent, CardHeader, Typography, Avatar } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  card: {
+    marginTop: '100px',
+    overflow: 'visible',
+  },
+  avatar: {
+    width: '200px',
+    height: '200px',
+    margin: '16px',
+    marginTop: '-100px',
+  },
+  headerContainer: {
+    display: 'flex',
+  },
+  cardHeader: {
+    width: '70%',
+  },
+});
+
+const ProductDescription = (props) => {
+  const classes = useStyles();
+  const product = props.product;
+  console.log(product.img);
+  return (
+    <Card className={classes.card}>
+      <div className={classes.headerContainer}>
+        <CardHeader
+          title={product.name}
+          subheader={product.weight}
+          className={classes.cardHeader}
+        />
+        <Avatar src={`/images/${product.img}`} className={classes.avatar} />
+      </div>
+
+      <CardContent>
+        <Typography paragraph>{product.description}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductDescription;
